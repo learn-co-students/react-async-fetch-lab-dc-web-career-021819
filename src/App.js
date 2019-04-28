e2185972fe34ca5ba37b7c2fe9d02c8f319910d9
seanlucius <seanlucius22@icloud.com>
2019-04-28 16:30:18 -0400

    react-async-fetch-lab-done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      astronauts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          astronauts: data.people
+        });
+      });
+  }
+
+  render() {
+    return <div>{this.state.astronauts}</div>;
+  }
+}
+
+export default App;
